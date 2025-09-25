a811c565b7fc73933150e6d76b9394db9c012e7c
Andrea To Chip <andreatochip@yahoo.com>
2025-09-25 17:09:23 +0800

    Div and JS Edits

--- Newsletter Sign Up with Success Message/script.js
+++ Newsletter Sign Up with Success Message/script.js
@@ -1,1 +1,16 @@
+
+
 const submit = documentGetByElementID("submit");
+
+function switchDivs() {
+  const div1 = document.getElementsByClassName('.container');
+  const div2 = document.getElementByClassName('.container2');
+
+  if (div1.style.display === 'none') {
+    div1.style.display = 'block'; 
+    div2.style.display = 'none';  
+  } else {
+    div1.style.display = 'none';  
+    div2.style.display = 'block'; 
+  }
+}
