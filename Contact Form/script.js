e7d7a461a848644469d58e77e9a748549a7b244a
Andrea To Chip <andreatochip@yahoo.com>
2025-10-10 18:56:45 +0800

    ID names and const elements in JS

--- Contact Form/script.js
+++ Contact Form/script.js
@@ -1,8 +1,5 @@
 
 
-
-
-
 // FOR SUCCESS STATE POPUP
 
 // Script for Success State Pop-up
@@ -31,10 +28,26 @@ if (openPopupBtn) {
 closePopupBtn.addEventListener('click', closePopup);
 popupOverlay.addEventListener('click', closePopup); // Close when clicking outside the popup
 
+
+
+// Submit data
+
+function submitInput() {
+  const submit = document.getElementById('submit');
+  const submitClicked = submit.isClicked;
+}
+
+
 // FOR ERROR MESSAGES IN EACH INPUT
 
+
+// Check if inputs are correct
 function validateInput() {
-    const inputField = document.getElementById('myInput');
+    const fname = document.getElementById('fname');
+    const lname = document.getElementById('lname');
+    const email = document.getElementById('email');
+    const query = document.getElementById('email');
+    const textarea = document.getElementById('textarea');
     const errorMessageElement = document.getElementById('errorMessage');
     const inputValue = inputField.value.trim(); // Get and trim the input value
 
@@ -51,5 +64,19 @@ function validateInput() {
         // Input is valid
         alert('Input is valid!');
         inputField.classList.remove('error-border'); // Remove error styling
+
+    }
+}
+
+// Check if consent checkbox is checked
+function consentInput() {
+
+    const consent = document.getElementById("consent"); 
+    const isChecked = consent.checked; 
+
+    if (isChecked) {
+      if (submitClicked) {
+
+      }
     }
 }
